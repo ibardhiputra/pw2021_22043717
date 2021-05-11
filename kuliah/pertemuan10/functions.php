f51bceaba34cf658beb614cea3238b7d8a94cf81
ibardhiputra <ardhi86@gmail.com>
2021-05-11 11:21:15 +0800

    koneksi dan tampil data

---
+++ kuliah/pertemuan10/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'pw_22043717');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+  $result = mysqli_query($conn, $query);
+
+  // jika hasil querynya 1 data saja
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
